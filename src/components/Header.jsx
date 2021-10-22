0281b142c685a219c6a4acf48bd65f107e81c163
ricardoggz <ricardo_ggzicm@outlook.com>
2021-10-22 16:36:28 -0500

    Header terminado

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "../styles/header.css"
+
+export const Header = () => {
+    return (
+        <header>
+            <div className="flex-container menu-container">
+                <a href="../index.html"><h2 className="logo">LearnJS</h2></a>
+                <nav>
+                    <input type="checkbox" id="menu"/>
+                    <label htmlFor="menu" className="bar-btn">
+                        <i class="bi bi-list"></i>
+                    </label>
+                    <ul className="list-menu">
+                        <li><a href="../index.html">Inicio</a></li>
+                        <li><a href="../index.html">Contacto de creador</a></li>
+                        <li><a href="../index.html">Repositorio</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
